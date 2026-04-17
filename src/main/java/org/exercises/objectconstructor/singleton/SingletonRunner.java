5e5a19bdc0fa95aae4e946cfacf18ded76b6793e
KristinaTesthub <143405909+KristinaTesthub@users.noreply.github.com>
2026-04-17 13:33:50 +0300

    Сделаны правки по 3.5 & 3.6 Класс Object, Конструкторы

---
+++ src/main/java/org/exercises/objectconstructor/singleton/SingletonRunner.java
@@ -0,0 +1,10 @@
+package org.exercises.objectconstructor.singleton;
+
+public class SingletonRunner {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        System.out.println(s1 == s2);
+    }
+}
